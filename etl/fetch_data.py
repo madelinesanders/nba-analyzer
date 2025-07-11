8f7259c26747596a0dafa7ec52c0eed616f2eb85
madelinesanders <63517681+madelinesanders@users.noreply.github.com>
2025-07-10 20:55:04 -0400

    Fixed logging path for EC2

--- etl/fetch_data.py
+++ etl/fetch_data.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('/Users/madelinesanders/Projects/nba-analyzer/etl/etl.log'),
+        logging.FileHandler('etl/etl.log'),
         logging.StreamHandler()
     ]
 )
